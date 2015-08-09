c6a7030a7be8439dbbd81175fc09487ab56ceacf
Jofulius <jon.futch@gmail.com>
2015-08-09 00:12:08 -0600

    GAnimComponent - Getting rid of D3D drawing.  Updating space formatting a bit.

--- Gyro/Source/Components/GAnimComponent.cpp
+++ Gyro/Source/Components/GAnimComponent.cpp
@@ -1,13 +1,13 @@
 #include "GAnimComponent.h"
 #include "Actor/ComponentManager.h"
 #include "../../../Renderer/Source/ResourceTypes/Mesh.h"
-#include "../../../Renderer/Source/RenderManager.h"
 #include "Animation/GAnimationUtil.h"
 #include "Animation/GSkeleton.h"
 #include "Animation/GAnimClip.h"
 #include "GRenderComponent.h"
 #include "Utility/Clock.h"
 #include "Animation/GAnimNode.h"
+#include "Renderer/GDebugDraw.h"
 
 const GHashedString GAnimComponent::m_typeName = "Anim";
 u32 GAnimComponent::m_typeId = ComponentManager::GetNextTypeId();
@@ -17,17 +17,17 @@ IActorComponent* GAnimComponent::Create()
 	return new GAnimComponent();
 }
 
-void GAnimComponent::Initialize(GActorHandle actor, LuaPlus::LuaObject& i_obj)
+void GAnimComponent::Initialize( GActorHandle actor, LuaPlus::LuaObject& i_obj )
 {
 	m_skeletonInstance = NULL;
 
 	LuaPlus::LuaObject& nodes = i_obj["AnimNodes"];
-	for (LuaPlus::LuaTableIterator it(nodes); it; it.Next())
+	for( LuaPlus::LuaTableIterator it( nodes ); it; it.Next() )
 	{
 		LuaPlus::LuaObject obj = it.GetValue();
 		GAnimNode* node = GAnimNode::Create(obj);
-		if (node)
-			m_nodeMap.Set(GHashedString::Hash(node->m_clip->m_Name), node);
+		if( node )
+			m_nodeMap.Set( GHashedString::Hash(node->m_clip->m_Name), node );
 	}
 
 	//m_clip = GAnimationUtil::LoadAnimClip("Avatar/Player/GoblinRun.banm");
@@ -35,15 +35,15 @@ void GAnimComponent::Initialize(GActorHandle actor, LuaPlus::LuaObject& i_obj)
 	m_currentNode->Begin();
 	m_prevNode = NULL;
 	m_clipTime = 0.0f;
-	m_debugDraw = false;
+	m_debugDraw = true;
 }
 
-void GAnimComponent::PlayAnim(const char* i_name)
+void GAnimComponent::PlayAnim( const char* i_name )
 {
 	GHashNode<u32, GAnimNode*>* node = m_nodeMap[GHashedString::Hash(i_name)];
-	if (node)
+	if( node )
 	{
-		if (node->m_data->m_blendInEndFrame > node->m_data->m_trimStart && node->m_data != m_currentNode )
+		if( node->m_data->m_blendInEndFrame > node->m_data->m_trimStart && node->m_data != m_currentNode )
 			m_prevNode = m_currentNode;
 
 		m_currentNode = node->m_data;
@@ -51,56 +51,47 @@ void GAnimComponent::PlayAnim(const char* i_name)
 	}
 }
 
-void GAnimComponent::Update(GActorHandle actor)
+void GAnimComponent::Update( GActorHandle actor )
 {
-	if (m_skeletonInstance == NULL)
+	if( m_skeletonInstance == NULL )
 	{
-		GRenderComponent* comp = GetComponent<GRenderComponent>(actor);
-		if (!comp)
+		GRenderComponent* comp = GetComponent<GRenderComponent>( actor );
+		if( !comp )
 			return;
 
 		Gyro::RendererObject* obj = comp->GetRendererObject();
-		if (!obj)
+		if( !obj )
 			return;
 
 		m_skeletonInstance = obj->m_entity->m_mesh->m_reference->rm_Reference.m_skeletonInstance;
 	}
 	else
 	{
-		if ( m_currentNode )
-			m_currentNode->Update(m_skeletonInstance, g_Clock::Get().SecondsSinceLastFrame());
+		if( m_currentNode )
+			m_currentNode->Update( m_skeletonInstance, g_Clock::Get().SecondsSinceLastFrame() );
 
-		if ( m_prevNode )
-			m_prevNode->Update(m_skeletonInstance, g_Clock::Get().SecondsSinceLastFrame());
+		if( m_prevNode )
+			m_prevNode->Update( m_skeletonInstance, g_Clock::Get().SecondsSinceLastFrame() );
 
 		//else
 		//	m_skeletonInstance->BuildBindPose();
 
-		if (m_debugDraw)
+		if( m_debugDraw )
 		{
 			// loop through first 30 or so bones and draw...
-			D3DXVECTOR3 first;
-			D3DXVECTOR3 second;
-			D3DXCOLOR	color(1.0f, 0.0f, 0.0f, 1.0f);
-
 			GActor* a = GActor::FromHandle(actor);
 			GVector3 worldPos = a->m_position;
-			worldPos._x = worldPos._x + 50.0f;
+			worldPos._x = worldPos._x + 2.0f;
 			GMatrix4 rot = a->m_rotation;
-			//rot.PokeScale( GVector3( 5.0f, 5.0f, 5.0f ) );
 
-
-			for (int i = 1; i < m_skeletonInstance->m_Bones.Count(); i++)
+			for( int i = 1; i < m_skeletonInstance->m_Bones.Count(); i++ )
 			{
 				GVector3 toWorldFirst;
 				GVector3 toWorldSecond;
-				GMatrix4::OrientVector3(toWorldFirst, m_skeletonInstance->m_Bones[i].m_AbsTranslation, rot);
-				GMatrix4::OrientVector3(toWorldSecond, m_skeletonInstance->m_Bones[m_skeletonInstance->m_Bones[i].m_ParentId].m_AbsTranslation, rot);
-				( toWorldFirst + worldPos ).ToD3DX(first);
-				( toWorldSecond + worldPos ).ToD3DX(second);
-				//DEBUG_PRINT("%d - %f %f %f\n", i, toWorldFirst.x(), toWorldFirst.y(), toWorldFirst.z());
+				GMatrix4::OrientVector3( toWorldFirst, m_skeletonInstance->m_Bones[i].m_AbsTranslation, rot );
+				GMatrix4::OrientVector3( toWorldSecond, m_skeletonInstance->m_Bones[m_skeletonInstance->m_Bones[i].m_ParentId].m_AbsTranslation, rot );
 
-				g_RenderManager.m_lines.AddLine(first, second, 0.002f, 0.002f, 1.5f, color);
+				GDebugDraw::DrawLine( toWorldFirst + worldPos, ( toWorldSecond + worldPos ), 0.002f, 0.002f, GDebugDraw::RED );
 			}
 		}
 	}
@@ -110,23 +101,22 @@ void GAnimComponent::Update(GActorHandle actor)
 
 void GAnimComponent::Evaluate()
 {
-	if (m_currentNode)
+	if( m_currentNode )
 	{
-		if (m_prevNode)
+		if( m_prevNode )
 		{
 			float into = m_currentNode->CalculateBlendInto();
-			if (into < 1.0f)
-				GAnimationUtil::Evaluate_AnimClipBlend(m_skeletonInstance, m_currentNode->m_clip, m_prevNode->m_clip, m_currentNode->m_currTime, m_prevNode->m_currTime, into);
+			if( into < 1.0f )
+				GAnimationUtil::Evaluate_AnimClipBlend( m_skeletonInstance, m_currentNode->m_clip, m_prevNode->m_clip, m_currentNode->m_currTime, m_prevNode->m_currTime, into );
 			else
 			{
-				GAnimationUtil::Evaluate_AnimClip(m_skeletonInstance, m_currentNode->m_clip, m_currentNode->m_currTime );
+				GAnimationUtil::Evaluate_AnimClip( m_skeletonInstance, m_currentNode->m_clip, m_currentNode->m_currTime );
 				m_prevNode = NULL;
 			}
 		}
 		else
-			GAnimationUtil::Evaluate_AnimClip(m_skeletonInstance, m_currentNode->m_clip, m_currentNode->m_currTime );
+			GAnimationUtil::Evaluate_AnimClip( m_skeletonInstance, m_currentNode->m_clip, m_currentNode->m_currTime );
 	}
-
 
 	m_skeletonInstance->EvaluateFullInstance();
 	m_skeletonInstance->CreateBoneMatrices();
--- Gyro/Source/Components/GAnimComponent.h
+++ Gyro/Source/Components/GAnimComponent.h
@@ -1,5 +1,5 @@
-#ifndef __GANIM_COMPONENT_H_
-#define __GANIM_COMPONENT_H_
+#ifndef __GYRO_COMPONENTS_GANIMCOMPONENT_H_
+#define __GYRO_COMPONENTS_GANIMCOMPONENT_H_
 
 #include "Actor/GActor.h"
 #include "Actor/IActorComponent.h"
@@ -16,34 +16,27 @@ public:
 	static const GHashedString			m_typeName;
 	static u32							m_typeId;
 
-	GAnimComponent()
-	{
-
-	}
+	GAnimComponent(){ }
 	virtual								~GAnimComponent(void) { }
 
-	virtual void						Update(GActorHandle actor);
-	virtual void						EndUpdate(GActorHandle actor) {}
+	virtual void						Update( GActorHandle i_actor );
+	virtual void						EndUpdate( GActorHandle i_actor ) {}
 	virtual bool						HasUpdate() const { return true; }
 	virtual bool						HasEndUpdate() const { return false; }
 	virtual IActorComponent*			Create();
-	virtual void						Initialize(GActorHandle actor, LuaPlus::LuaObject& i_obj);
+	virtual void						Initialize( GActorHandle actor, LuaPlus::LuaObject& i_obj );
 	virtual GHashedString				Name(void) const { return m_typeName; }
 	void								PlayAnim( const char* i_name );
 	void								Evaluate();
 
-	void					DebugDrawBones();
+	void								DebugDrawBones();
 
-	float					m_clipTime;
-	//GAnimClip*				m_clip;
-
-
+	float						m_clipTime;
 	bool						m_debugDraw;
 	GSkeletonInstance*			m_skeletonInstance;
 	GHashMap<u32, GAnimNode*>	m_nodeMap;
 	GAnimNode*					m_currentNode;
 	GAnimNode*					m_prevNode;
-
 };
 
 #endif
